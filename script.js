8fe028f165eb310de0c4470226a409eed42e98a4
ismail halliru muhammad <ismailhm888@gmail.com>
2023-06-25 15:44:29 +0200

    Validate contact form in js file

--- script.js
+++ script.js
@@ -162,5 +162,42 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     if (email.value.trim() !== email.value.trim().toLowerCase()) {
         showErrorMessage();  
+  } else{
+    alert("Form submitted successfully!");
   }
 });
+
+
+// LOCAL STORAGE
+const formEntries = JSON.parse(localStorage.getItem('contactForm')) || {
+    name: '',
+    email: '',
+    message: '',
+  };
+  
+  const saveInputValues = () => {
+    name.value = formEntries.name;
+    email.value = formEntries.email;
+    message.value = formEntries.message;
+  };
+  
+  saveInputValues();
+  
+  const LocalStorage = () => {
+    localStorage.setItem('contactForm', JSON.stringify(formEntries));
+  };
+  
+  name.addEventListener('input', () => {
+    formEntries.name = name.value;
+    LocalStorage();
+  });
+
+  email.addEventListener('input', () => {
+    formEntries.email = email.value;
+    LocalStorage();
+  });
+
+  message.addEventListener('input', () => {
+    formEntries.message = message.value;
+    LocalStorage();
+  });
